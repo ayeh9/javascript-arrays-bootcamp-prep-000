65ab8104e5337a9034284763adf27616943bd29c
passionate-coder-1337 <awyeh450471@gmail.com>
2018-11-27 01:22:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,15 @@
 function addElementToBeginningOfArray(m,n){
-  var array = [];
-  array = m.unshift(n);
+  var array = m;
+  array.unshift(n);
   return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(m,n){
   m.unshift(n);
   return m;
+}
+
+function addElementToEndOfArray(m,n){
+  var array = [];
+  array = m.push
 }
